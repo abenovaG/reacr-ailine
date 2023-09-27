1a92c3acc6972e47e90669b6de2cb579ca7c82a1
abenovaG <abenovagulsha@gmail.com>
2023-09-27 18:10:58 +0600

    added some...

--- src/Ailine/Ailine.js
+++ src/Ailine/Ailine.js
@@ -1,13 +1,22 @@
 import blackLogo from '../image/blackLogo.png'
+import shortArrow from '../image/shortArrow.png'
+import x from '../image/x.png'
 import {useTranslation} from "react-i18next";
 import '../css/reset.css'
 import '../css/ailine.css'
-
 import '../18n'
+import {useState} from "react";
 
 export const Ailine = () => {
-
+    const [openInfo, setOpenInfo] = useState(false)
     const {t, i18n} = useTranslation()
+
+    const seeInfo = () => {
+        setOpenInfo(true);
+    }
+    const closeInfo = () => {
+        setOpenInfo(false);
+    }
 
     const changeLanguage = (language) => {
         i18n.changeLanguage(language);
@@ -23,8 +32,8 @@ export const Ailine = () => {
                 <div className="header">
                     <img className="logoPng" src={blackLogo} alt="AilineLogo"/>
                         <ul className="contactUs">
-                            <li className="contact">{t("contactUs")}</li>
                             <li className={"contact"}>{t("address")}</li>
+                            <li className={"contact"}>+996 700 199 910</li>
                             <li className={"contact"}>+996 700 199 910</li>
                             <li className={"contact"}>
                                 <a href="https://www.instagram.com/ailine.kg/">{t("ceo")}</a>
@@ -33,34 +42,50 @@ export const Ailine = () => {
                 </div>
                 <div className="content">
                     <h1>{t("title")}</h1>
-                    <h3>We provide services and sustainable solutions in the field of web & mobile development</h3>
+                    <h3>{t("article")}</h3>
                     <ul>
-                        <li>Web & Mobile Development</li>
-                        <li>Publishing Platforms for Media Organizations</li>
-                        <li>Open-Data, Social-Media and SMS Integration</li>
-                        <li>Website & App Support, SEO</li>
+                        <li>{t("development")}</li>
+                        <li>{t("publishing")}</li>
+                        <li>{t("open")}</li>
+                        <li>{t("seo")}</li>
                     </ul>
                     <h2>{t("h2")}</h2>
+                    <h3>{t("proudly")}</h3>
                     <ul>
-                        <li>{t("kenguroo")}</li>
                         <li>
-                            <a href="https://alfa-story.kg/">{t("alfa")}</a>
+                            <a href="https://kenguroo.com/">{t("kenguroo")}</a>
                         </li>
                         <li>
-                            <a href="https://bitok.kg/">{t("bitok")}</a>
-                        </li>
-                        <li>
-                            <a href="https://dlxlimousine.com/">{t("dlx")}</a>
-                        </li>
-                        <li>{t("agrimatco")}</li>
-                        <li>
-                            <a href="https://t-express.us/">{t("express")}</a>
+                            <a href="https://play.google.com/store/apps/details?id=com.makers.agrimatco">{t("agrimatco")}</a>
                         </li>
                         <li>
                             <a href="https://kvadrat.kg/">{t("kvadrat")}</a>
                         </li>
+                        <img className="arrow" title="see more" onClick={seeInfo} src={shortArrow} alt="arrow"/>
+                        {openInfo && (
+                            <div className="modal">
+                                <ul>
+                                    <img className="xImage" onClick={closeInfo} src={x} alt="x"/>
+                                    <li>
+                                        <a href="https://alfa-story.kg/">{t("alfa")}</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://bitok.kg/">{t("bitok")}</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://dlxlimousine.com/">{t("dlx")}</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="https://t-express.us/">{t("express")}</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        )}
+
                     </ul>
                     <h2>{t("title2")}</h2>
+                    <h3>{t("NGO")}</h3>
                     <ul>
                         <li>
                             <a href="https://www.kumtor.kg/">{t("kumtor")}</a>
